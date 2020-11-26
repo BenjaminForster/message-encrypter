98ac7fd333f143e1a0be15cd8d5b0f13977844ca
Benjamin <56231581+BenjaminForster@users.noreply.github.com>
2020-11-26 18:43:00 +0000

    Create msgev1.py
    Version 1.

---
+++ msgev1.py
@@ -0,0 +1,36 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+special = '[@_!#$%^&*()<>?/\|}{~:]1234567890'
+keySpecial = 'abcdefghijklmnopqrstuvwxyz[@_!#$%^&*()<>?/\|}{~:]'
+newMessage = ''
+
+message = input('Please enter a message to encrypt: ').lower()
+if len(message) < 2:
+    print('Message needs to be at least 2 characters, exiting program')
+    exit()
+
+specialInMessage = [c for c in special if c in message]
+if specialInMessage:
+    print('Invalid character found in message, exiting program')
+    exit()
+    
+key = input('Please enter a key from 1-26: ')
+SpecialInMessage = [c for c in keySpecial if c in key]
+if SpecialInMessage:
+    print('Invalid character found in key, exiting program')
+    exit()
+key = int(key)
+if key < 1 or key > 26:
+    print('Invalid character found in key, exiting program')
+    exit()
+
+for character in message:
+  if character in alphabet:
+    position = alphabet.find(character)
+    newPosition = (position + key) % 26
+    newCharacter = alphabet[newPosition]
+    newMessage += newCharacter
+  else:
+    newMessage += character
+
+print('Your new message is: ', newMessage)
+exit()
